565964e8a6074360a2d364f42dde9c7f1adc44d8
testninza <tester876905@gmail.com>
2024-09-11 20:32:40 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -3,7 +3,7 @@ app.get('/user', (req, res) => {
     const query = `SELECT * FROM users WHERE id = ${userId}`; // Vulnerable to SQL Injection
     db.query(query, (err, result) => {
         if (err) {
-            res.status(500).send('Error');
+            res.status(500).send('Erccror');
         } else {
             res.json(result);
         }
